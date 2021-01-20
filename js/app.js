ac04886a875f49ae6c8290505cd46404580ad00d
webmaaster <wecodewetest@gmail.com>
2021-01-20 13:32:52 -0500

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,13 @@
+
+var a = 5000;//define
+var b = 5;
+var c = a+b;
+var d = a-b;
+var e = a*b;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+window.alert(e);
+document.write("<h1>Welcome to 1st day of the course</h1>");
